f5fb670f227a467497b043920b6db1389cd1d3ea
Phùng Quang Anh <tenzovn@gmail.com>
2024-08-03 22:20:42 +0700

    Setup Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 function App() {
-	return <></>;
+	return (
+		<Layout>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/blog" element={<Blog />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</Layout>
+	);
 }
 
 export default App;
